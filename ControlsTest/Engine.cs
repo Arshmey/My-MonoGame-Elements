33c2de47bf085c3f3ec809481603a8a6b4efb2b9
Arshmey <dima.gnedenkov@gmail.com>
2025-11-21 03:13:54 +0300

    Auto LoadContent Maked method for auto load content.

--- ControlsTest/Engine.cs
+++ ControlsTest/Engine.cs
@@ -4,6 +4,9 @@ using ControlsTest.Drivers;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Concurrent;
+
+using System.IO;
 
 namespace ComtrolsTest
 {
@@ -14,6 +17,9 @@ namespace ComtrolsTest
         private Rectangle screen;
         private MouseDriver mouse;
         private KeyboardDriver keyboard;
+
+        private ConcurrentDictionary<string, SpriteFont> fonts;
+        private ConcurrentDictionary<string, Texture2D> textures;
 
         private Button bNewGame;
         private TextBox textBox;
@@ -31,21 +37,22 @@ namespace ComtrolsTest
             screen = Window.ClientBounds;
             mouse = new MouseDriver();
             keyboard = new KeyboardDriver();
+
+            fonts = new ConcurrentDictionary<string, SpriteFont>();
+            textures = new ConcurrentDictionary<string, Texture2D>();
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
-            SpriteFont font = Content.Load<SpriteFont>("Font");
-            SpriteFont fontSmall = Content.Load<SpriteFont>("FontSmall");
-            Texture2D btnTexture = Content.Load<Texture2D>("Button");
-            Texture2D txtBoxTexture = Content.Load<Texture2D>("DialogBar");
-            Texture2D paperTexture = Content.Load<Texture2D>("Paper");
-            bNewGame = new Button(font, Color.White, "New Game", btnTexture, new Vector2(16, 256), Color.White);
-            textBox = new TextBox(font, Color.White, "Text Test in Box Hahahaha", txtBoxTexture,
-                new Vector2(0, screen.Height - txtBoxTexture.Height), Color.White);
-            paper = new TextBox(font, Color.Black, "It's a paper", paperTexture, new Vector2(256, 32), Color.White);
+            LoadFiles();
+            bNewGame = new Button(GetFont("FontSmall"), Color.White, "New Game", GetTexture("Button"),
+                new Vector2(16, 256), Color.White);
+            textBox = new TextBox(GetFont("FontSmall"), Color.White, "Text Test in Box Hahahaha", GetTexture("DialogBar"),
+                new Vector2(0, screen.Height - textures["DialogBar"].Height), Color.White);
+            paper = new TextBox(GetFont("FontSmall"), Color.Black, "It's a paper", GetTexture("Paper"),
+                new Vector2(256, 32), Color.White);
 
             bNewGame.Action += () => { Environment.Exit(0); };
             textBox.Action += () => { textBox.text = "LMAO"; };
@@ -71,6 +78,41 @@ namespace ComtrolsTest
             paper.GetDraw(spriteBatch);
             spriteBatch.End();
             base.Draw(gameTime);
+        }
+
+        private void LoadFiles()
+        {
+            foreach (string path in Directory.GetDirectories(Content.RootDirectory))
+            {
+                string foldername = new DirectoryInfo(path).Name;
+
+                foreach (string filePath in Directory.GetFiles(Path.Combine(Content.RootDirectory, foldername)))
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(filePath);
+
+                    var file = Content.Load<object>(Path.Combine(foldername, fileName));
+
+                    switch (file)
+                    {
+                        case SpriteFont:
+                            fonts.TryAdd(fileName, file as SpriteFont);
+                            break;
+                        case Texture2D:
+                            textures.TryAdd(fileName, file as Texture2D);
+                            break;
+                    }
+                }
+            }
+        }
+
+        public SpriteFont GetFont(string nameFont)
+        {
+            return fonts[nameFont];
+        }
+
+        public Texture2D GetTexture(string nameTexture)
+        {
+            return textures[nameTexture];
         }
     }
 }
